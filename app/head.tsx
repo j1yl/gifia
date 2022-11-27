b70e6c2477cfa9cf935acb63d085473dac172199
Joe Lee <lefodev@gmail.com>
2022-11-27 01:14:44 -0800

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>GIFIA</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="An application made using the GIPHY API"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
