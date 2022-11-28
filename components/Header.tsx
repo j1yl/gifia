7a602c3aa8da3f418843f988ff316b77bafda370
Joe Lee <lefodev@gmail.com>
2022-11-27 23:05:53 -0800

    Testing form

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ let letters = ["G", "i", "f", "i", "a"];
 const Header = () => {
   return (
     <>
-      <nav className="flex fixed top-0 items-center w-full h-max max-w-6xl mx-auto text-4xl md:text-6xl font-bold px-4 md:px-16 py-4 md:py-16">
+      <nav className="flex items-center w-full h-max max-w-6xl mx-auto text-4xl md:text-6xl font-bold px-4 md:px-16 py-4 md:py-16">
         <Link href="/">
           {letters.map((item, index) => (
             <span key={index}>{item}</span>
--- components/Search.tsx
+++ components/Search.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <>
+      <div className="bg-slate-300 mx-auto items-center w-max">
+        <input type="text" name="search" />
+        <input type="submit" name="search" />
+      </div>
+    </>
+  );
 };
 
 export default Search;
