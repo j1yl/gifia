8595c33e4f44f04dea4fc52e7a721ada2ec9ff64
Joe Lee <lefodev@gmail.com>
2022-12-04 09:38:54 -0800

    Try working with data

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Search />
-      <Content query={"Test"} />
+      <Content query={"cats"} />
     </>
   );
 }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: [
+      "media0.giphy.com",
+      "media1.giphy.com",
+      "media2.giphy.com",
+      "media3.giphy.com",
+      "media4.giphy.com",
+      "media5.giphy.com",
+      "media6.giphy.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
