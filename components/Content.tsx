26bd8e480319e5aba6f79ea535beee368038552c
Joe Lee <lefodev@gmail.com>
2022-12-03 19:51:09 -0800

    Add api endpoint

--- components/Content.tsx
+++ components/Content.tsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const APIKEY = process.env.GIPHY_API;
 
-const getData = async () => {
-  const res = await fetch("endpoint");
+// https://api.giphy.com/v1/gifs/search?
+
+const getData = async (query: string) => {
+  const limit = 25;
+  const res = await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${query}&limit=${limit}&offset=0&rating=g&lang=en`
+  );
 
   if (!res.ok) {
     throw new Error("Failed to get data");
@@ -12,8 +17,8 @@ const getData = async () => {
   return res.json();
 };
 
-const Content = () => {
-  const data = getData();
+const Content = ({ query }: { query: string }) => {
+  const data = getData(query);
   console.log(data);
 
   return <main></main>;
